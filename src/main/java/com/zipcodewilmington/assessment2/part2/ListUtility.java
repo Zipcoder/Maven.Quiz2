568795ada74705f3c2b13d91766ac34f153bf245
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-10 10:33:42 -0400

    ListUtility passing all tests

--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -1,29 +1,55 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListUtility {
-    public Boolean add(int i) {
-        return null;
+
+    private List<Integer> theList;
+
+    public ListUtility(){
+        theList = new ArrayList<Integer>();
+    }
+
+    public Boolean add(Integer i) {
+
+        return theList.add(i);
     }
 
     public Integer size() {
-        return null;
+        return theList.size();
     }
 
     public List<Integer> getUnique() {
-        return null;
+        List<Integer> uniqueList = new ArrayList<>();
+        for(Integer integer : theList){
+            if(!uniqueList.contains(integer)){
+                uniqueList.add(integer);
+            }
+        }
+        return uniqueList;
     }
 
     public String join() {
-        return null;
+        if(theList.size()==0){return "";}
+
+        StringBuilder sbuild = new StringBuilder(theList.get(0).toString());
+        for(int i = 1; i < theList.size(); i++){
+            sbuild.append(", ");
+            sbuild.append(theList.get(i));
+        }
+        return sbuild.toString();
     }
 
     public Integer mostCommon() {
-        return null;
+        ArrayUtility arrayUtility = new ArrayUtility();
+        return arrayUtility.mostCommon(theList.toArray(new Integer[theList.size()]));
     }
 
     public Boolean contains(Integer valueToAdd) {
-        return null;
+        return theList.contains(valueToAdd);
     }
+
 }
