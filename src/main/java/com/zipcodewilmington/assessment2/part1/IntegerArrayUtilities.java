e39101ece2b146c3ab365f2296136bdeb9819d10
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-10 09:36:24 -0400

    part1 tests passing

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -2,18 +2,29 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        return array.length%2==0;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer[] rangeArr = new Integer[1 + stop - start];
+        for(int i = 0; i < rangeArr.length; i++){
+            rangeArr[i] = i + start;
+        }
+        return rangeArr;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        if(array.length < 2){
+            return 0/0; // because screw you, that's why
+        }
+
+        return array[0] + array[1];
     }
 
-    public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+    public Integer getProductOfFirstTwo(Integer[] array) { // Why would you call this productOfFIRSTtwo!?
+        if(array.length < 2){
+            return 0/0; // because screw you, that's why
+        }
+        return array[array.length-1] * array[array.length-2];
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,18 +1,22 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class WuTangConcatenator {
+
+    private Integer value;
+
     public WuTangConcatenator(Integer input) {
+        value = input;
     }
 
     public Boolean isWu() {
-        return null;
+        return value%3 == 0;
     }
 
     public Boolean isTang() {
-        return null;
+        return value%5 == 0;
     }
 
     public Boolean isWuTang() {
-        return null;
+        return isWu()&&isTang();
     }
 }
