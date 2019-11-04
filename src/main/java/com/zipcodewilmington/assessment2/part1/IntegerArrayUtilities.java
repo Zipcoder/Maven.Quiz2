e8d160b6d29dc930c80aa3707983710592a778b3
Mike King <michkin32@gmail.com>
2019-11-04 10:29:06 -0500

    starting part 2

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -2,18 +2,28 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+
+        return array.length % 2 == 0;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer[] newRange = new Integer[Math.abs(start + stop)];
+        int holdStart = start;
+        for (int i = start; i <= stop + 1; i++){
+                newRange[i] = holdStart;
+                holdStart++;
+            }
+
+
+        return newRange;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+
+        return array[0] + array[1];
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        return array[array.length - 1] * array[array.length- 2];
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -2,10 +2,40 @@ package com.zipcodewilmington.assessment2.part2;
 
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+
+        Integer counter = 0;
+        for (Integer i : array1){
+            counter++;}
+        for (Integer i : array2){
+            counter++;
+        }
+
+
+        Integer[] completeArray = new Integer[counter];
+        Integer secondCounter = 0;
+        for (Integer i : array1){
+            completeArray[secondCounter] = i;
+            secondCounter++;
+        }
+        for (Integer i : array2){
+            completeArray[secondCounter] = i;
+            secondCounter++;
+        }
+        return completeArray;
+
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
+        Integer[] indexToRotate = new Integer[array.length];
+        Integer counter = 0;
+        for (int i = 0; i < indexToRotate.length; i++) {
+            Integer integer = indexToRotate[i];
+
+        }
+        for (int i = 0; i < index; i++){
+            indexToRotate[counter] = array[i];
+            counter++;
+        }
         return null;
     }
 
