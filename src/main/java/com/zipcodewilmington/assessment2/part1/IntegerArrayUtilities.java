f38f5091d747fd9f90b748d7aa8eb1945ad3f8a3
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-10.local>
2019-11-04 11:38:53 -0500

    completed part 1 of Quiz2

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,33 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import org.w3c.dom.ranges.Range;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        if (array.length % 2 == 0)
+            return true;
+        else
+            return false;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        List<Integer> rangeBetweenStartStop = IntStream.rangeClosed(start, stop)
+                .boxed().collect(Collectors.toList());
+        return rangeBetweenStartStop.toArray(new Integer[0]);
     }
+
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
-    }
 
-    public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        return array[0] + array[1];
     }
-}
+        public Integer getProductOfFirstTwo (Integer[]array){
+            return array[array.length-2] * array[array.length-1];
+        }
+    }
