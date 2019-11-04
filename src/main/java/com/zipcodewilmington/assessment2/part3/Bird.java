9333c8233ec3c08caa6884e43347946d644aac53
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-3.local>
2019-11-04 13:50:01 -0500

    part 3 done

--- src/main/java/com/zipcodewilmington/assessment2/part3/Bird.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/Bird.java
@@ -1,15 +1,21 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class Bird {
-    public String move() {
-        return null;
-    }
+public abstract class Bird implements Animal {
+
+    String migration;
+
+   public abstract String move();
+   public abstract String color();
+   public abstract int getSpeed();
 
     public void setMigrationMonth(String expected) {
+        this.migration = expected;
 
     }
 
     public String getMigrationMonth() {
-        return null;
+        return migration;
     }
+
+
 }
--- src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
@@ -1,4 +1,19 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class RedRobin {
+public class RedRobin extends Bird {
+
+    @Override
+    public String move() {
+        return "fly";
+    }
+
+    @Override
+    public String color() {
+        return "red";
+    }
+
+    @Override
+    public int getSpeed() {
+        return 2;
+    }
 }
