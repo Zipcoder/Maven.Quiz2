1ca967510aff276c06008261dbc7a719ace819b2
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-11-04 11:04:52 -0500

    33 tests passing, first commit

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,18 +1,23 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class WuTangConcatenator {
+    // declare a variable to use
+    public Integer userInput;
+
     public WuTangConcatenator(Integer input) {
+        // assign new variable to input
+        this.userInput = input;
     }
 
     public Boolean isWu() {
-        return null;
+        return userInput % 3 == 0;
     }
 
     public Boolean isTang() {
-        return null;
+        return userInput % 5 == 0;
     }
 
     public Boolean isWuTang() {
-        return null;
+        return userInput % 15 == 0;
     }
 }
