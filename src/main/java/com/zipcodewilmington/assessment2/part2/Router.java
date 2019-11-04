87fcae98141a6ced050ce21da2fe99f94db6df88
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-04 13:30:25 -0500

    Add logic for router size

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,24 +1,36 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Router {
-    private String result = "";
+    private List<String> result = new ArrayList<>();
+    //private String result = "";
 
     public void add(String path, String controller) {
-        result = controller;
+        result.add(controller);
     }
 
     public Integer size() {
-        return null;
+        return result.size();
     }
 
     public String getController(String path) {
-        return result;
+        String returnedString = "";
+        String listElement = "";
+
+        for (int i = 0; i < result.size(); i++){
+            listElement = result.get(i);
+            returnedString += listElement ;
+        }
+        return (result.size() > 0) ? returnedString : null;
     }
 
     public void update(String path, String studentController) {
     }
 
     public void remove(String path) {
-        result = null;
+        result.clear();
     }
 }
