ec068954d051c23418f65018d7d16f8f646f9af3
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-11-04 11:44:26 -0500

    35 passing

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -14,6 +14,26 @@ public class ArrayUtility {
     }
 
     public Integer mostCommon(Integer[] array) {
-        return null;
+        // declare variables needed
+        int counter =  0;
+        int counterTemp = 0;
+        Integer highest = 0;
+
+        // inner loop checks for highest number before outer loop goes thru
+        for (int i = 0 ; i <= array.length -1 ; i ++ ) {
+            for (int j = 0; j <= array.length -1; j++) {
+                if (array[i] == array[j]){
+                    counterTemp++;
+                }
+                // after inner loop checks for MC num,
+                if(counterTemp >= counter){
+                    counter = counterTemp;
+                    counterTemp = 0;
+                    highest = array[i];
+                }
+
+            }
+        }
+        return highest;
     }
 }
