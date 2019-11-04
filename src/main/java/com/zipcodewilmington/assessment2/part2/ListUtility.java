88e5215c62898d693a670117cc5186d667da7fce
deltagphys <cty.jgates@gmail.com>
2019-11-04 10:23:09 -0500

    all but one in section 2

--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -1,34 +1,68 @@
 package com.zipcodewilmington.assessment2.part2;
 
-import java.util.List;
+import java.util.*;
 
 public class ListUtility {
-    private List<Integer> =
+    private ArrayList<Integer> list;
 
     public ListUtility() {
-        this.
+        this.list = new ArrayList<Integer>();
     }
-    public Boolean add(int i) {
-        return null;
+
+    public Boolean add(Integer i) {
+        try {
+            this.list.add(i);
+            return true;
+        } catch (NullPointerException e) {
+            return false;
+        }
     }
 
     public Integer size() {
-        return null;
+        return this.list.size();
     }
 
     public List<Integer> getUnique() {
-        return null;
+        HashSet<Integer> unique = new HashSet<Integer>(this.list);
+
+        return new ArrayList<Integer>(unique);
     }
 
     public String join() {
-        return null;
+        String[] sArray = new String[this.list.size()];
+        for (int i = 0; i < this.list.size(); i++) {
+            sArray[i] = this.list.get(i).toString();
+        }
+
+        return String.join(", ",sArray);
     }
 
     public Integer mostCommon() {
-        return null;
+
+        Integer[] array = this.list.toArray(new Integer[this.list.size()]);
+        return mostCommon(array);
+    }
+
+    public Integer mostCommon(Integer[] array) {
+        int max = 0;
+        TreeMap<Integer, Integer> counts = new TreeMap<Integer,Integer>();
+        for (Integer num : array) {
+            counts.put(countOccurrence(array,num),num);
+        }
+        return counts.get(counts.lastKey());
+    }
+
+    public Integer countOccurrence(Integer[] array1, Integer valueToEvaluate) {
+        int count = 0;
+        for (Integer num : array1) {
+            if (num == valueToEvaluate){
+                count++;
+            }
+        }
+        return count;
     }
 
     public Boolean contains(Integer valueToAdd) {
-        return null;
+        return this.list.contains(valueToAdd);
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,20 +1,32 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.HashMap;
+
 public class Router {
+
+    private HashMap<String,String> map;
+
+    public Router() {
+        this.map = new HashMap<String,String>();
+    }
+
     public void add(String path, String controller) {
+        map.put(path, controller);
     }
 
     public Integer size() {
-        return null;
+        return this.map.size();
     }
 
     public String getController(String path) {
-        return null;
+        return this.map.get(path);
     }
 
     public void update(String path, String studentController) {
+        add(path,studentController);
     }
 
     public void remove(String path) {
+        this.map.remove(path);
     }
 }
