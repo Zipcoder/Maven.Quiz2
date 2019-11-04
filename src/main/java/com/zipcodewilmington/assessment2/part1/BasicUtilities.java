d4f13b5e77ac3c9e1d7ce0ac665b9e08eca499f6
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-11-04 09:43:38 -0500

    Complete basicUtilities

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -1,19 +1,25 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
-    public Boolean isGreaterThan5(Integer value) {
-        return null;
+
+    public Boolean isGreaterThan5(Integer value){
+        return value > 5;
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        return value < 7;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        Boolean isBetween;
+        if (valueToEvaluate >= 5 && valueToEvaluate <= 7) {
+            isBetween = true;
+        } else {
+            isBetween = false;
+        } return isBetween;
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+        return string.charAt(0) == character;
     }
 }
