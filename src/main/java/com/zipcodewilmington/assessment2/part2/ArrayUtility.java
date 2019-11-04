549d674215195487316e37b2035e00bf24d10d52
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-04 10:12:32 -0500

    Add logic for count occurance

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.Arrays;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
         return null;
@@ -10,7 +12,12 @@ public class ArrayUtility {
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
-        return null;
+        int result = 0;
+
+        if (Arrays.binarySearch(array1,valueToEvaluate) >=0) return result;
+        if (Arrays.binarySearch(array2,valueToEvaluate) >=0) return result;
+
+        return result;
     }
 
     public Integer mostCommon(Integer[] array) {
