6ea24dbe68d3fccc438193920afd03027af0cdca
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-04 10:14:05 -0500

    finished chunk

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -2,7 +2,20 @@ package com.zipcodewilmington.assessment2.part2;
 
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] mergedArray = new Integer[array1.length + array2.length];
+        Integer counter = 0;
+        for(int i = 0; i < array1.length;i++){
+            mergedArray[counter] = array1[i];
+            counter++;
+        }
+
+        for(int i = 0; i < array2.length; i++){
+            mergedArray[counter] = array2[i];
+            counter++;
+        }
+
+
+        return mergedArray;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
