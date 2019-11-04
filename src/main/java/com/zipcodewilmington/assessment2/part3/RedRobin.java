ff5f354a781d130f0a67869fa87d901b948a7f93
ushanil <ush.gopi@gmail.com>
2019-11-04 12:29:29 -0500

    Part 3

--- src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
@@ -1,4 +1,24 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class RedRobin {
+public class RedRobin extends Bird
+{
+    private String migMonth;
+
+    @Override
+    public int getSpeed() {
+        return 10;
+    }
+
+    @Override
+    public String color() {
+        return "red";
+    }
+
+    @Override
+    public String getMigrationMonth() {
+        return migMonth;
+    }
+    public void setMigrationMonth(String migrationMonth) {
+        this.migMonth = migrationMonth;
+    }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
@@ -1,4 +1,6 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class SpeedComparator {
+public class SpeedComparator
+{
+
 }
