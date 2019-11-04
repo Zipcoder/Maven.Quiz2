54c1540e3952adf5ed67d2b6c15953f3869f3b70
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-04 14:16:42 -0500

    Add logic for blue jay

--- src/main/java/com/zipcodewilmington/assessment2/part3/BlueJay.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/BlueJay.java
@@ -1,4 +1,14 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class BlueJay {
+public class BlueJay extends Bird implements Animal{
+    public String move() {
+        return "fly";
+    }
+    public  int getSpeed(){
+        return 0;
+    }
+
+    public String color(){
+        return "blue";
+    }
 }
