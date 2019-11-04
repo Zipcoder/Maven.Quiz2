f207bebe7bc130d74e2aa01a0d34edc487b884a4
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-11-04 11:42:50 -0500

    Complete

--- src/main/java/com/zipcodewilmington/assessment2/part3/Horse.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/Horse.java
@@ -1,4 +1,17 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class Horse{
+public class Horse implements Animal{
+    String color = "brown";
+
+    public String color() {
+        return color;
+    }
+
+    public int getSpeed() {
+        return 40;
+    }
+
+    public String move() {
+        return "gallop";
+    }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
@@ -1,4 +1,21 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class RedRobin {
+public class RedRobin extends Bird implements Animal {
+    String migrationMonth;
+
+    public String color() {
+        return "red";
+    }
+
+    public int getSpeed() {
+        return 10;
+    }
+
+    public String getMigrationMonth() {
+        return this.migrationMonth;
+    }
+
+    public void setMigrationMonth(String month) {
+        this.migrationMonth = month;
+    }
 }
