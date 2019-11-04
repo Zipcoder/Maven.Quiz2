67f1da17fae421b0d27f15925e5b053c673a97ed
Brandon Chambers <brandonmchambers36@gmail.com>
2019-11-04 09:50:47 -0500

    part one with one remeaining question

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -2,18 +2,39 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        if(value >= 5){
+            return true;
+        }
+       return false;
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        if(value <= 7){
+            return true;
+        }
+        return false;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        if(valueToEvaluate >= 5 && valueToEvaluate <= 7 ){
+            return true;
+        }
+        return false;
     }
-
+//not done
     public Boolean startsWith(String string, Character character) {
+        /*
+        String[] strArr = new String[string.length()];
+
+        for(int i = 0; i < strArr.length; i++){
+            strArr[count] = string[i];
+        }
+
+        if(string[0] == character){
+            return true;
+        }
+
+         */
         return null;
     }
 }
