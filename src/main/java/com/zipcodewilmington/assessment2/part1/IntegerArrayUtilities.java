d5c44ea2207b2dbfc10d6869e0ce70d727cc739c
Ben Coon <bencoon@zipcoders-MacBook-Pro-22.local>
2019-11-04 11:02:12 -0500

    Array Utilities Finished

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -2,11 +2,21 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        return array.length % 2 == 0;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+
+        Integer length = Math.abs(start - stop) + 1;
+        Integer counter = 0;
+
+        Integer[] arr = new Integer[length];
+
+        for(int i = start; i <= stop; i++) {
+            arr[counter] = i;
+            counter++;
+        }
+        return arr;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,8 +1,20 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.Arrays;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] array3 = new Integer[array1.length + array2.length];
+
+        for(int j = 0; j < array1.length; j++) {
+            array3[j] = array1[j];
+        }
+
+        for(int i = array1.length; i < array3.length; i++) {
+            array3[i] = array2[i];
+        }
+
+        return array3;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
