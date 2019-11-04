8668452a4884d384ac6fd943a7b9db7adc2e88b2
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-04 14:06:12 -0500

    Add logic for to string

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,36 +1,53 @@
 package com.zipcodewilmington.assessment2.part2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Router {
-    private List<String> result = new ArrayList<>();
-    //private String result = "";
+
+    private Map<String,String> mapResult = new HashMap<String,String>();
+
 
     public void add(String path, String controller) {
-        result.add(controller);
+
+        mapResult.put(path,controller);
     }
 
     public Integer size() {
-        return result.size();
+        return mapResult.size();
     }
 
     public String getController(String path) {
         String returnedString = "";
         String listElement = "";
 
-        for (int i = 0; i < result.size(); i++){
-            listElement = result.get(i);
-            returnedString += listElement ;
+        for (int i = 0; i < mapResult.size(); i++){
+            listElement = mapResult.get(path);
+            returnedString += listElement;
         }
-        return (result.size() > 0) ? returnedString : null;
+
+        return (mapResult.size() > 0) ? returnedString : null;
     }
 
     public void update(String path, String studentController) {
     }
 
     public void remove(String path) {
-        result.clear();
+
+        mapResult.remove(path);
+    }
+
+    @Override
+    public String toString() {
+        String [] keyValues = new String [mapResult.size()];
+        StringBuilder result = new StringBuilder();
+
+        mapResult.keySet().toArray(keyValues);
+
+        for (int i = (keyValues.length - 1); i >= 0; i--){
+            result.append(keyValues[i]).append(mapResult.get(keyValues[i])).append('\n');
+        }
+        result.toString();
+
+        return result.toString();
     }
 }
