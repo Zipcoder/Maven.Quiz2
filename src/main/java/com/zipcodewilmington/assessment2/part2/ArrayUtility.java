ba0301869ff9462af4b80722a067a8fe7667536f
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-04 10:32:41 -0500

    Add logic for merge

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,10 +1,26 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import com.j256.ormlite.stmt.query.In;
+
 import java.util.Arrays;
 
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        int newArrayLenght = array1.length + array2.length;
+        Integer [] result = new Integer[newArrayLenght];
+        int currentIndex = 0;
+
+        for (int i = 0; i < array1.length; i++){
+                result[i] = array1[i];
+                currentIndex++;
+        }
+
+        for (int i = 0; i < array2.length; i++){
+            result[currentIndex] = array2[i];
+            currentIndex++;
+        }
+
+        return result;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
@@ -14,8 +30,12 @@ public class ArrayUtility {
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
         int result = 0;
 
-        if (Arrays.binarySearch(array1,valueToEvaluate) >=0) return result;
-        if (Arrays.binarySearch(array2,valueToEvaluate) >=0) return result;
+        for (int i = 0; i < array1.length; i++){
+            if (array1[i] == valueToEvaluate) result++;
+        }
+        for (int i = 0; i < array2.length; i++){
+            if (array2[i] == valueToEvaluate) result++;
+        }
 
         return result;
     }
