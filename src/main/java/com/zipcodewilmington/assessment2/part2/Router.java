3446fcb26c22d686e6fbb26159494c9e7ef8b566
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-11-04 11:31:37 -0500

    Complete part 2

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,21 +1,29 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Router {
+    Map<String, String> controllerMap = new HashMap();
 
     public void add(String path, String controller) {
+        controllerMap.put(path, controller);
     }
 
     public Integer size() {
-        return null;
+        return controllerMap.size();
     }
 
     public String getController(String path) {
-        return null;
+        return controllerMap.get(path);
     }
 
     public void update(String path, String studentController) {
+        controllerMap.replace(path, studentController);
     }
 
     public void remove(String path) {
+        controllerMap.remove(path);
     }
+
 }
