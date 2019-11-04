97e4a7a1b342a195e5c3dc71d0687b4a944f0677
wes <wes.jones1597@gmail.com>
2019-11-04 10:01:52 -0500

    rotate test passing

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -2,11 +2,25 @@ package com.zipcodewilmington.assessment2.part2;
 
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] newArray = new Integer[array1.length+array2.length];
+        for (int i = 0; i < newArray.length; i++) {
+            if (i < array1.length) {
+                newArray[i] = array1[i];
+            } else {
+                newArray[i] = array2[i-array1.length];
+            }
+        }
+        return newArray;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
-        return null;
+        Integer[] newArray = new Integer[array.length];
+        for (int i = 0; i < array.length; i++) {
+            Integer newIndex = i + index;
+            if (newIndex >= array.length) newIndex -= array.length;
+            newArray[i] = array[newIndex];
+        }
+        return newArray;
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
