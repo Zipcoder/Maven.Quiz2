e31a2374140b10ec8b4cdb8ce87afa451475a554
Brian Patterson <brian@zipcoders-MacBook-Pro-5.local>
2019-11-04 10:11:51 -0500

    part 1 complete

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -2,18 +2,22 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        return array.length % 2 == 0;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer[]numRange = new Integer[Math.abs((stop-start)+1)];
+        for (int i = 0; i < numRange.length; i++) {
+            numRange[i]= start+i;
+        }
+        return numRange;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        return array[0] + array[1];
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        return array[array.length-1] * array[array.length-2];
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,18 +1,26 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class WuTangConcatenator {
+    Integer input;
+
     public WuTangConcatenator(Integer input) {
+        this.input = input;
+
+        isWu();
+        isTang();
+        isTang();
+
     }
 
     public Boolean isWu() {
-        return null;
+        return input%3 == 0;
     }
 
     public Boolean isTang() {
-        return null;
+        return input%5 == 0;
     }
 
     public Boolean isWuTang() {
-        return null;
+        return input%15 == 0;
     }
 }
