9eba51c3c6230ef90f696eaf65b1bf4f7afcf012
David Trombello <david@MacBook-Pro-8.localdomain>
2019-11-04 11:15:20 -0500

    More tests working

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -1,19 +1,70 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.regex.Pattern;
+
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        if (value > 5) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        if (value < 7) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        if (valueToEvaluate < 5 || valueToEvaluate > 7) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
-    }
+        String lowerCase;
+        lowerCase= string.toLowerCase();
+        char [] chars = lowerCase.toCharArray();
+        char firstLetter = chars[0];
+        char lowerCaseChar;
+        lowerCaseChar = character.toLowerCase(character);
+        System.out.println(firstLetter);
+        System.out.println(lowerCaseChar);
+        if (lowerCaseChar == firstLetter)
+        { return true; }
+            else {return false;}
+
+        //System.out.println(character);
+        //if(firstLetter == )
+
+//        character = character.toLowerCase(character);
+//        if(string.substring(0,1).toLowerCase().equals(character))
+//        {return true;}
+//        else {return false;}
+
+        //String firstLetter = character.toString();
+
+
+
+
+//        boolean startsWith = false;
+//        string = string.toLowerCase();
+//        character = character.toLowerCase(character);
+//        System.out.println(character);
+//        String firstLetter = string.substring(0, 1);
+//        System.out.println(firstLetter);
+//        if (firstLetter.equals(character)) {
+//            startsWith = true;
+//        }
+//        return startsWith;
+  }
+
 }
+
+
--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,39 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.ArrayList;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        if (array.length % 2 ==0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        int arrayLength = (stop - start) + 1;
+            if ( arrayLength < 0){
+                arrayLength = arrayLength * (-1);
+                arrayLength += 1;
+            }
+        System.out.println(arrayLength);
+        Integer[] theRange = new Integer [arrayLength];
+        //ArrayList<Integer> myRange = new ArrayList<>();
+
+        for (int i = 0; i <=stop; i++) {
+            theRange[i] = start + i;
+        }
+            return theRange;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        return array[0] + array [1];
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        int lastNumIndex = array.length - 1;
+        return array[lastNumIndex] * array [lastNumIndex - 1];
+
     }
 }
