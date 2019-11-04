bd849cfc2b397f688f93f0c9a1a3df31fb9d355b
Ben Coon <bencoon@zipcoders-MacBook-Pro-22.local>
2019-11-04 10:26:57 -0500

    Finished basic utilites

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -1,21 +1,41 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
-    public Boolean isGreaterThan5(Integer value) {
 
+    private Boolean greaterThanFive = false;
+    private Boolean lessThanSeven = false;
+    private Boolean between = false;
 
-        return null;
+    public void evaluator(Integer value) {
+        if(value >= 5)
+            this.greaterThanFive = true;
+        if(value <= 7)
+            this.lessThanSeven = true;
+        if(greaterThanFive && lessThanSeven)
+            this.between = true;
     }
 
+    public Boolean isGreaterThan5(Integer value) {
+        evaluator(value);
+        return greaterThanFive; }
+
     public Boolean isLessThan7(Integer value) {
-        return null;
+        evaluator(value);
+        return lessThanSeven;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        evaluator(valueToEvaluate);
+        return between;
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+        if (character <= 65 || character >= 90) {
+            return character == string.charAt(0) || character == string.charAt(0) + 32;
+        }
+        if (character <= 97 || character >= 122) {
+            return character == string.charAt(0) || character == string.charAt(0) - 32;
+        }
+        return false;
     }
 }
