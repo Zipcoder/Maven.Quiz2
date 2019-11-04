36b42a3c68db12d1348f53b575de481cd5992d37
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-10.local>
2019-11-04 10:04:01 -0500

    completed Basic Util

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -1,19 +1,28 @@
 package com.zipcodewilmington.assessment2.part1;
-
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        return value >= 5;
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        return value <= 7;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+
+        if (valueToEvaluate <= 7 && valueToEvaluate >= 5)
+            return true;
+        else
+        return false;
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+        if (string.toLowerCase().charAt(0) == character)
+            return true;
+        else if( string.toUpperCase().charAt(0) == character)
+        return true;
+
+        else return false;
     }
+
 }
