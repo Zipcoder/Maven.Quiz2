6f229c236f5058afa85261106e04eca2663da230
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-04 12:26:25 -0500

    Add logic for unique list

--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -1,10 +1,16 @@
 package com.zipcodewilmington.assessment2.part2;
 
-import java.util.List;
+import java.util.*;
 
 public class ListUtility {
+    List<Integer> result = new ArrayList<Integer>();
+
     public Boolean add(int i) {
-        return null;
+        boolean yesorno = false;
+
+        result.add(i);
+        yesorno = result.contains(i);
+        return yesorno;
     }
 
     public Integer size() {
@@ -12,7 +18,20 @@ public class ListUtility {
     }
 
     public List<Integer> getUnique() {
-        return null;
+        Integer [] tempArray = new Integer[result.size()];
+        result.toArray(tempArray);
+        Set<Integer> tempSet = new TreeSet<Integer>();
+
+        for (int i = 0; i < tempArray.length; i++){
+            tempSet.add(tempArray[i]);
+        }
+
+        result.clear();
+        for (Integer x : tempSet){
+            result.add(x);
+        }
+
+        return result;
     }
 
     public String join() {
