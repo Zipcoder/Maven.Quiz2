6d47a681bfdb53e91dd132717ab6216cea1623df
SandyPham823 <sandydupham@gmail.com>
2019-11-04 10:52:33 -0500

    Finished all of part 1 utilities, now on WuTangConcatenator

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -2,18 +2,35 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        if (value >= 5) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        if (value <= 7) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        if ((valueToEvaluate % 5 == 0 || valueToEvaluate % 6 == 0 || valueToEvaluate % 7 == 0) && (valueToEvaluate != 0)) {
+            return true;
+
+        } else {
+            return false;
+        }
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+        char[] ch = string.toCharArray();
+        for (int i = 0; i < string.length(); i++)
+            if (ch[0] == character || ch[0] == character.toUpperCase(character))
+                return true;
+        return false;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,27 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        if (array.length % 2 == 0)
+        return true;
+
+    return false;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        int[] range = IntStream.rangeClosed(start, stop).toArray();
+        Integer[] finalRange = IntStream.of(range).boxed().toArray(Integer[]::new);
+        return finalRange;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        return array[0] + array[1];
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        return array[array.length - 1] * array[array.length - 2];
     }
 }
