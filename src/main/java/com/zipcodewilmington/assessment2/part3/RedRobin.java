608cc6657d44baa1cbaac13299e2ebb652f16770
wes <wes.jones1597@gmail.com>
2019-11-04 11:57:52 -0500

    part 3 tests passing

--- src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
@@ -1,4 +1,19 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class RedRobin {
+public class RedRobin extends Bird {
+    private String migrationMonth;
+    public String color() {
+        return "red";
+    }
+    public int getSpeed() {
+        return 10;
+    }
+
+    @Override
+    public String getMigrationMonth() {
+        return migrationMonth;
+    }
+    public void setMigrationMonth(String migrationMonth) {
+        this.migrationMonth = migrationMonth;
+    }
 }
