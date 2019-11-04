19031de61d2f47661bc64236e34e4acf8a12f1d7
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-04 10:21:59 -0500

    finished ArrayUtils

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,5 +1,10 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import com.sun.tools.javac.util.ArrayUtils;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
         Integer[] mergedArray = new Integer[array1.length + array2.length];
@@ -23,10 +28,36 @@ public class ArrayUtility {
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
-        return null;
+        Integer occurrenceCounter = 0;
+        for(int i = 0; i < array1.length;i++) {
+            if (array1[i].equals(valueToEvaluate)) {
+                occurrenceCounter++;
+            }
+        }
+
+            for(int i = 0; i < array2.length;i++){
+                if(array2[i].equals(valueToEvaluate)){
+                    occurrenceCounter++;
+                }
+        }
+        return occurrenceCounter;
     }
 
     public Integer mostCommon(Integer[] array) {
-        return null;
+
+        int count = 0;
+        int mostCommon = array[0];
+        int temp = 0;
+        int tempCount = 0;
+        for(int i = 0; i <= array.length - 1;i++){
+            temp = array[i];
+            tempCount = Collections.frequency(Arrays.asList(array), temp);
+        }
+        if(tempCount > count){
+            mostCommon = temp;
+            count = tempCount;
+            tempCount = 0;
+        }
+        return mostCommon;
     }
 }
