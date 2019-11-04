25ddb610c0404ff2f972706cdbc332988093d39a
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-3.local>
2019-11-04 10:13:40 -0500

    part1 done

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,41 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.ArrayList;
+
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+
+        if(array.length == 0 || array.length % 2 == 0)
+            return true;
+
+        return false;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        for(int first = start; first <= stop; first++){
+            temp.add(first);
+        }
+
+        Integer[] result = new Integer[temp.size()];
+        result = temp.toArray(result);
+
+        return result;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+
+        Integer sumFirstTwo = array[0] + array[1];
+
+        return sumFirstTwo;
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+
+       Integer productLastTwo = array[array.length - 1] * array[array.length - 2];
+
+        return productLastTwo;
     }
 }
