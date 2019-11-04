a69e1894db9c9fc5bb08f025595303b033712330
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-04 14:23:30 -0500

    Add logic for red robin

--- src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
@@ -1,4 +1,26 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class RedRobin {
+public class RedRobin  extends Bird implements Animal{
+    String migrationMonth = "";
+
+    public String move() {
+
+        return "fly";
+    }
+
+    public void setMigrationMonth(String expected) {
+        this.migrationMonth = expected;
+    }
+
+    public String getMigrationMonth() {
+
+        return migrationMonth;
+    }
+
+    public int getSpeed(){
+        return 0;
+    }
+    public String color(){
+        return "red";
+    }
 }
