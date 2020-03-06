fa334f8ef8dc40a0c45aac8a35069de3bcf3edbb
vle <obentoka@gmail.com>
2020-03-06 10:37:21 -0500

    almsot done ListUtil

--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -1,29 +1,61 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListUtility {
-    public Boolean add(int i) {
-        return null;
+
+    List<Integer> list = new ArrayList<>();
+
+    //CHANGED INT TO INTEGER TO ACCEPT NULL
+    public Boolean add(Integer i) {
+        return list.add(i);
     }
 
     public Integer size() {
-        return null;
+        return list.size();
     }
 
     public List<Integer> getUnique() {
-        return null;
+        List<Integer> retList = new ArrayList<>();
+        retList.add(list.get(0));
+        for(Integer e : list){
+            if(!retList.contains(e))
+                retList.add(e);
+        }
+        return retList;
     }
 
     public String join() {
-        return null;
+        StringBuilder retString = new StringBuilder();
+        for(Integer e : list){
+            if(list.indexOf(e) != 0)
+                retString.append(", " + e);
+            else
+                retString.append(e + "");
+        }
+        return retString.toString();
     }
 
+    //NULL ERROR
     public Integer mostCommon() {
-        return null;
+        int maxCount = 0;
+        Integer retInt = 0;
+        for (int i = 0; i < list.size(); i++) {
+            int count = 1;
+            for (int j = 1; j < list.size(); j++) {
+                if(list.get(i).equals(list.get(j)))
+                    count++;
+            }
+            if(count > maxCount){
+                retInt = list.get(i);
+                maxCount = count;
+            }
+        }
+        return retInt;
     }
 
     public Boolean contains(Integer valueToAdd) {
-        return null;
+        return list.contains(valueToAdd);
     }
 }
