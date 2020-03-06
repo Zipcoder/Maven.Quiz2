bf90f4028dbc0b802777e7c48155e604187f47bd
JeremyMccray <jeremy.mccray1337@gmail.com>
2020-03-06 11:29:27 -0500

    this test is busted

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,20 +1,32 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.Hashtable;
+
 public class Router {
+    Hashtable<String,String> test = new Hashtable<>();
     public void add(String path, String controller) {
+        test.put(path,controller);
     }
 
     public Integer size() {
-        return null;
+        return test.size();
     }
 
     public String getController(String path) {
-        return null;
+
+        return test.get(path);
     }
 
     public void update(String path, String studentController) {
+        test.replace(path,studentController);
     }
 
     public void remove(String path) {
+        test.remove(path);
+    }
+
+    @Override
+    public String toString() {
+        return ""+ test.keySet() ;
     }
 }
