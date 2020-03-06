d5db816e23b8bc65589523e30380edb4ab120ad0
vle <obentoka@gmail.com>
2020-03-06 09:53:02 -0500

    IntegerArrayUtil done

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -6,18 +6,20 @@ public class IntegerArrayUtilities {
     }
 
     public Integer[] range(int start, int stop) {
-        Integer[] retArray = new Integer[stop-start];
-//        for (int i = 0; i < ; i++) {
-//
-//        }
-        return null;
+        int length = stop - start + 1;
+        Integer[] retArray = new Integer[length];
+        for (int i = 0; i < length; i++, start++) {
+            retArray[i] = start;
+        }
+        return retArray;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
         return array[0] + array[1];
     }
 
+    //HAS TYPO IN NAME. PRODUCT OF LAST TWO NOT FIRST TWO
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return array[0] * array[1];
+        return array[array.length-2] * array[array.length-1];
     }
 }
