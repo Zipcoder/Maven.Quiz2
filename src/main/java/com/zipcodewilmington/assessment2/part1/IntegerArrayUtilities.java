c8b718c8f7e783bdce159b88b8e7ab1f260dd473
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-03-06 09:52:04 -0500

    int arr utils done

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,31 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+
+        return array.length%2==0;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        List<Integer> list= new ArrayList<>();
+        for (int i = start; i < stop+1; i++) {
+            list.add(i);
+        }
+        Integer[] result = new Integer[list.size()];
+        result = list.toArray(result);
+        return result;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+
+        return array[0]+array[1];
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+
+        return array[array.length-1]*array[array.length-2];
     }
 }
