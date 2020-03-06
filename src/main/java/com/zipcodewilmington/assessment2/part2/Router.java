85395c219efdb12855ad5d7ae56af7735f493534
vle <obentoka@gmail.com>
2020-03-06 10:49:27 -0500

    Router toString doesnt work

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,20 +1,36 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Router {
+
+    Map<String, String> router = new HashMap<>();
+
     public void add(String path, String controller) {
+        router.put(path, controller);
     }
 
     public Integer size() {
-        return null;
+        return router.size();
     }
 
     public String getController(String path) {
-        return null;
+        return router.get(path);
     }
 
     public void update(String path, String studentController) {
+        router.replace(path, studentController);
     }
 
     public void remove(String path) {
+        router.remove(path);
+    }
+
+    @Override
+    public String toString() {
+        return "Router{" +
+                "router=" + router +
+                '}';
     }
 }
