49a4de2d7178c2e9148fd31b9d5fe325213d65d1
hlin <Zxsaqw12@&$>
2020-03-06 13:58:56 -0500

    Pass one more test class

--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -1,6 +1,7 @@
 package com.zipcodewilmington.assessment2.part2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ListUtility {
@@ -24,16 +25,26 @@ public class ListUtility {
     }
 
     public String join() {
-        return null;
+        Integer[] result = new Integer[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return Arrays.toString(result).replaceAll("\\[|]|", "");
     }
 
     public Integer mostCommon() {
-        return null;
+        ArrayUtility arrayUtility = new ArrayUtility();
+        Integer[] result = new Integer[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return arrayUtility.mostCommon(result);
+
     }
 
     public Boolean contains(Integer valueToAdd) {
         if (valueToAdd == null) {
-            return null;
+            return true;
         }
         return list.contains(valueToAdd);
     }
