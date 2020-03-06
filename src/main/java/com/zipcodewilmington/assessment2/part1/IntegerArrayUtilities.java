08a4d2213dcdf9d1907e027098f1bb0e3bcfda70
Sandy Setiawan <ssetiawan@zipcodersMBP21.localdomain>
2020-03-06 11:30:47 -0500

    all compiled

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,6 +1,8 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class IntegerArrayUtilities {
@@ -13,10 +15,18 @@ public class IntegerArrayUtilities {
 
 
     public Integer[] range(int start, int stop) {
-        //   int[] range = IntStream.iterate(1, n -> n + 1).limit(10).toArray();
-        //  }
-        return null;
-    }
+        List<Integer> result = new ArrayList<>();
+        for (int i = start; i <= stop; i++) {
+            result.add(i);
+        }
+
+        Integer[] finalResult = new Integer[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            finalResult[i] = result.get(i);
+        }
+        return finalResult;
+
+}
 
         public Integer getSumOfFirstTwo ( Integer[]array){
             return array[0] + array[1];
