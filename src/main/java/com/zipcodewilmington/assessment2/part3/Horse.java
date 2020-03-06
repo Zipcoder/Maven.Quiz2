4dbd354a5a5a36f8efe8511636d441dbd265a8d3
Ujjwal Shrestha <ushrestha@zipcoders-MBP-5.localdomain>
2020-03-06 11:30:56 -0500

    Need to work later

--- src/main/java/com/zipcodewilmington/assessment2/part3/Horse.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/Horse.java
@@ -1,4 +1,20 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class Horse{
+public class Horse implements Animal{
+    String move;
+    String color;
+
+    public Horse() {
+        this.move="gallop";
+        this.color="brown";
+    }
+
+    @Override
+    public String move() { return move; }
+
+    @Override
+    public String color() { return color; }
+
+    @Override
+    public int getSpeed() { return 20; }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
@@ -1,4 +1,20 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class RedRobin {
+public class RedRobin extends Bird{
+
+    String migrationMonth;
+
+    @Override
+    public int getSpeed() { return 8; }
+
+    @Override
+    public String color() { return "red"; }
+
+    @Override
+    public void setMigrationMonth(String migrationMonth) { this.migrationMonth = migrationMonth; }
+
+    @Override
+    public String getMigrationMonth() { return migrationMonth; }
+
+
 }
