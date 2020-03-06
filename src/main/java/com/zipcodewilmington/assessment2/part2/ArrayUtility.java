56f45e076496c6c459a1e62e3230d83ba9d509bf
April Howard <ahoward@zipcodes-MacBook-Pro-5.local>
2020-03-06 11:19:25 -0500

    merge test pass

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,8 +1,18 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.ArrayList;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        ArrayList<Integer> result = new ArrayList<>();
+        for (Integer to : array1) {
+            result.add(to);
+
+        } for (Object to : array2) {
+            result.add((Integer) to);
+        }
+        Integer[] array = result.toArray(new Integer[array1.length + array2.length]);
+        return array;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
