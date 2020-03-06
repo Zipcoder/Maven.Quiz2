852905c3b486bb8620d7d3dea125867bed3b323e
aartikansal <aartilumb@gmail.com>
2020-03-06 11:36:42 -0500

    final submisiion

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,19 +1,63 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+
+        ArrayList<Integer>newList=new ArrayList<Integer>();
+
+
+                int newLength =array1.length+ array2.length;
+                newList.addAll(Arrays.asList(array1));
+                newList.addAll(Arrays.asList(array2));
+                return newList.toArray(new Integer[newLength]);
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
-        return null;
+
+        Integer[] newArray = new Integer[array.length];
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (i - index >= 0) {
+                newArray[i - index] = array[i];
+            } else {
+                newArray[i - index + array.length] = array[i];
+            }
+        }
+        return newArray;
+    }
+public static Integer getNumberOfOcurrences(Integer[] array, Integer valueToCount) {
+    int numOfOccurrences = 0;
+    for (Integer value : array) {
+        if (value == valueToCount)
+            numOfOccurrences = numOfOccurrences + 1;
+    }
+    return numOfOccurrences;
+}
+    public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
+//Integer[] mergedArray= merge[array1,array2];
+return null;//getNumberOfOcurrences(mergedArray, valueToEvaluate);
     }
 
-    public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
-        return null;
-    }
 
     public Integer mostCommon(Integer[] array) {
-        return null;
-    }
-}
+
+            //Integer[] occurrences = new Integer[array.length];
+           // for (int i = 0; i < array.length; i++) {
+              //  occurrences[i] = getNumberOfOccurrences(array, array[i]);
+           // }
+            // find most common (max # of occurrences) and return
+            //int max = occurrences[0];
+           // int maxIdx = 0;
+           // for (int i = 1; i < occurrences.length; i++) {
+               // if (occurrences[i] > max) {
+               //     max = occurrences[i];
+                 //   maxIdx = i;
+               // }
+           // }
+            return null;
+        }}
+
+
+
