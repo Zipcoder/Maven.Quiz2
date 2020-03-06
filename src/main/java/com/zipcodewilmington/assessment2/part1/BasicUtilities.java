a8f5197958042c9c563573732c1fdb3d629cc050
Zanetta Norris <znorris@zipcodes-MacBook-Pro-2.local>
2020-03-06 10:05:47 -0500

    added BasicUtilities code

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -2,18 +2,27 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        if (value > 5)
+            return true;
+        else return false;
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+//test and readme requirement differ. readme <7 & test <= 7
+        if (value <= 7)
+            return true;
+        else return false;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        if (valueToEvaluate > 5 && valueToEvaluate < 7)
+            return true;
+        else return false;
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+        if (string.substring(0, 1).equalsIgnoreCase(String.valueOf(character)))
+            return true;
+        else return false;
     }
 }
