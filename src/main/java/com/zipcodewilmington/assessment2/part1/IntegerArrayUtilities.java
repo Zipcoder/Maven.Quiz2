629f14669259fbfa5b61942079eec759c8a1c7c9
Mo Russ <mauriceruss302@gmail.com>
2020-03-06 10:18:30 -0500

    changes in WuTangConcatenator

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,12 +1,22 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.Arrays;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer[] array = new Integer[]{};
+        Integer[] copy2 = new Integer[]{};
+
+        return  copy2 = Arrays.copyOfRange(array, start, stop);
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -4,25 +4,25 @@ public class WuTangConcatenator {
     public Integer Wu;
     public Integer Tang;
     public Integer WuTang;
-    public Integer value = 0;
+    
     public WuTangConcatenator(Integer input) {
     }
 
     public Boolean isWu() {
-        if (value % 3 == 0) {
+        if (Wu % 3 == 0) {
             return true;
         }
         return false;
     }
 
     public Boolean isTang() {
-        if (value % 5 == 0) {
+        if (Tang % 5 == 0) {
             return true;
         }
         return false;
     }
     public Boolean isWuTang() {
-        if (value % 3 == 0 && value % 5 ==0) {
+        if (WuTang % 3 == 0 && WuTang % 5 ==0) {
             return true;
         }
         return false;
