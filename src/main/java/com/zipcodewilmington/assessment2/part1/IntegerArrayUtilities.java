ce4788f23c3279731163e014eb2a5a34fc385013
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-03-06 11:14:35 -0500

    finished most of part 1 and 2

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -2,18 +2,28 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        if (array.length % 2 == 0){
+            return true;
+        }
+        else return false;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer[] newArr = new Integer[stop - start + 1];
+        for (int i = 0; i < newArr.length ; i++) {
+            newArr[i] = start;
+            start++;
+        }
+        return newArr;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        Integer sum = array[0] + array[1];
+        return sum;
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        Integer sum = array[array.length - 1] * array[array.length-2];
+        return sum;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,19 +1,61 @@
 package com.zipcodewilmington.assessment2.part2;
 
+
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] newArr = new Integer[array1.length + array2.length];
+        for (int i = 0; i < array1.length ; i++) {
+            newArr[i] = array1[i];
+        }
+        for (int i = 0; i < array2.length ; i++) {
+            newArr[i + array1.length] = array2[i];
+        }
+        return newArr;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
-        return null;
+        Integer[] newArr = new Integer[array.length];
+        for (int i = 0; i < newArr.length ; i++) {
+            newArr[i] = array[index];
+            index++;
+            if(index == array.length){
+                index = 0;
+            }
+        }
+        return newArr;
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
-        return null;
+        Integer count = 0;
+
+        for (int i = 0; i < array1.length ; i++) {
+            if (array1[i] == valueToEvaluate){
+                count++;}
+        }
+        for (int i = 0; i < array2.length ; i++) {
+            if (array2[i] == valueToEvaluate){
+                count++;
+            }
+        }
+        return count;
     }
 
     public Integer mostCommon(Integer[] array) {
-        return null;
+        Integer mostOcc = 0;
+        Integer currCount = 0;
+        Integer occMost =0;
+        for (int i = 0; i <array.length ; i++) {
+            for (int j = i; j < array.length ; j++) {
+                if (array[i] == array[j]){
+                    currCount++;
+                }}
+            if(currCount > mostOcc){
+                mostOcc = currCount;
+                occMost = array[i];}
+                currCount = 0;
+
+        }
+        return occMost;
     }
 }
