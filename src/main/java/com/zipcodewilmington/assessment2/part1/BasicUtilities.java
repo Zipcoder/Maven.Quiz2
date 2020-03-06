2a8d743cb11e4b522ed1328dabd3279ac62c5b38
Nyoxide <nyoxidetwitter@gmail.com>
2020-03-06 09:49:27 -0500

    done part 1

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -2,18 +2,19 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+
+        return value >= 5;
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        return value <= 7;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        return valueToEvaluate > 4 && valueToEvaluate < 8;
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+        return ((Character)string.toLowerCase().charAt(0)).equals(Character.toLowerCase(character));
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,19 +1,74 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] newArr = new Integer[array1.length + array2.length];
+        int iter = 0;
+        for (Integer i : array1) {
+            newArr[iter] = i;
+            iter++;
+        }
+        for (Integer i : array2) {
+            newArr[iter] = i;
+            iter++;
+        }
+        return newArr;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
-        return null;
+        Integer[] newArr = new Integer[array.length];
+        int iter = 0;
+        for (int i = index; i < array.length; i++) {
+            newArr[iter] = array[i];
+            iter++;
+        }
+
+        for (int i = 0; i < index; i++) {
+            newArr[iter] = array[i];
+            iter++;
+        }
+        return newArr;
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
-        return null;
+        int sum = 0;
+        for (Integer i : array1) {
+            if (i == valueToEvaluate) {
+                sum++;
+            }
+        }
+        for (Integer i : array2) {
+            if (i == valueToEvaluate) {
+                sum++;
+            }
+        }
+        return sum;
     }
 
     public Integer mostCommon(Integer[] array) {
-        return null;
+        if (array.length < 1) {
+            throw new IllegalArgumentException("Cannot find most common element of empty array!");
+        }
+        Map<Integer, Integer> occ = new HashMap<>();
+        for (Integer i : array) {
+            if (occ.containsKey(i)) {
+                occ.put(i, occ.get(i) + 1);
+            } else {
+                occ.put(i, 1);
+            }
+        }
+
+        Integer highest = 0;
+        Integer num = array[0];
+        for (Map.Entry<Integer, Integer> i : occ.entrySet()) {
+            if (i.getValue() > highest) {
+                num = i.getKey();
+                highest = i.getValue();
+            }
+        }
+        return num;
     }
 }
