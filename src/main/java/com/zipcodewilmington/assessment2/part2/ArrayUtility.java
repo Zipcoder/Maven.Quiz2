b4074472ec0f892e4111c55c85072903164ec13c
James Wilkinson <jwilkinson@James-Wilkinsons-MacBook-Pro.local>
2020-03-06 10:03:54 -0500

    Partway through ArrayUtils

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,16 +1,55 @@
 package com.zipcodewilmington.assessment2.part2;
 
 public class ArrayUtility {
-    public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+    public Integer[] merge(Integer[] array1, Integer[] array2)
+    {
+        Integer[] output = new Integer[array1.length + array2.length];
+
+        for(Integer i = 0; i < array1.length; i++)
+        {
+            output[i] = array1[i];
+        }
+        for(Integer i = 0; i < array2.length; i++)
+        {
+            output[array1.length + i] = array2[i];
+        }
+
+        return output;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
-        return null;
+        Integer[] result = new Integer[array.length];
+
+        for(Integer i = 0; i < array.length; i++)
+        {
+            result[i] = array[(index + i) % array.length];
+        }
+
+
+
+        return result;
     }
 
-    public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
-        return null;
+    public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate)
+    {
+        Integer occurrences = 0;
+
+        for(Integer i = 0; i < array1.length; i++)
+        {
+            if(array1[i] == valueToEvaluate)
+            {
+                occurrences++;
+            }
+        }
+        for(Integer i = 0; i < array2.length; i++)
+        {
+            if(array2[i] == valueToEvaluate)
+            {
+                occurrences++;
+            }
+        }
+
+        return occurrences;
     }
 
     public Integer mostCommon(Integer[] array) {
