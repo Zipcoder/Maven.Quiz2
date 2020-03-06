c4ae38b9d077a031c3cd5418aa488b43263505c9
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-03-06 10:00:46 -0500

    part 1 done

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,34 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import com.j256.ormlite.stmt.query.In;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        if (array.length % 2 == 0){
+            return true;
+        }
+        return false;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+
+        int[] nums = java.util.stream.IntStream.rangeClosed(start, stop).toArray();
+
+        Integer[] newArray = new Integer[nums.length];
+
+        for (int i = 0; i < nums.length; i++){
+            newArray[i] = Integer.valueOf(nums[i]);
+        }
+
+        return newArray;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+
+        return array[0].intValue() + array[1].intValue();
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        return array[array.length - 2].intValue() * array[array.length - 1].intValue();
     }
 }
