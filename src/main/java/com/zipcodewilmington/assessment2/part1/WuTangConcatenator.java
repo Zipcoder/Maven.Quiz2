4da22535775791f86d87164832a36a459e8611b8
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-03-06 10:39:38 -0500

    merged array

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,18 +1,33 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import com.j256.ormlite.stmt.query.In;
+
 public class WuTangConcatenator {
     public WuTangConcatenator(Integer input) {
+
     }
 
     public Boolean isWu() {
+
+//        if ( % 3 == 0){
+//            return true;
+//        }
+//        return false;
         return null;
     }
 
     public Boolean isTang() {
+//        if (input % 5 == 0){
+//            return true;
+//        }
+//        return false;
         return null;
     }
 
     public Boolean isWuTang() {
-        return null;
+        if (isWu() == true && isTang() == true){
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,8 +1,17 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import com.sun.tools.hat.internal.model.JavaObjectArray;
+import com.sun.tools.javac.code.Attribute;
+import com.sun.tools.javac.util.ArrayUtils;
+import com.sun.tools.javac.util.List;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] merged = new Integer[array1.length + array2.length];
+
+        System.arraycopy(array1, 0, merged, 0, array1.length);
+        System.arraycopy(array2, 0, merged, array1.length, array2.length);
+        return merged;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
