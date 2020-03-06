bbd35ecf42c580411cceaa80b85ebdeef6de2efc
Mo Russ <mauriceruss302@gmail.com>
2020-03-06 09:51:46 -0500

    WuTangConcatenator class done

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,18 +1,30 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class WuTangConcatenator {
+    public Integer Wu;
+    public Integer Tang;
+    public Integer WuTang;
+    public Integer value = 0;
     public WuTangConcatenator(Integer input) {
     }
 
     public Boolean isWu() {
-        return null;
+        if (value % 3 == 0) {
+            return true;
+        }
+        return false;
     }
 
     public Boolean isTang() {
-        return null;
+        if (value % 5 == 0) {
+            return true;
+        }
+        return false;
     }
-
     public Boolean isWuTang() {
-        return null;
+        if (value % 3 == 0 && value % 5 ==0) {
+            return true;
+        }
+        return false;
     }
 }
