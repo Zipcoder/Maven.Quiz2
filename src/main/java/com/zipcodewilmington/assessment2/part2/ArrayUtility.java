4aeb8f9ca1e5a6023af845391b0c252fef70aeae
Zanetta Norris <znorris@zipcodes-MacBook-Pro-2.local>
2020-03-06 11:01:41 -0500

    added merge utility.

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,8 +1,18 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.Arrays;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer newArraySize = array1.length + array2.length;
+        Integer[] newArray = new Integer[newArraySize];
+        int a = array1.length;
+        int b = array2.length;
+        System.arraycopy(array1, 0, newArray, 0, a);
+        System.arraycopy(array2, 0, newArray, a, b);
+
+        return newArray;
+
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
