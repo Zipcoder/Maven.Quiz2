7b446262cdff56898d1471eac6f0701ad9c06a7b
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-06 09:48:35 -0500

    first part all tests pass

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,28 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class IntegerArrayUtilities {
+
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        return array.length % 2 == 0;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer[] ranger = new Integer[stop - start + 1];
+        int index = 0;
+        for (int i = stop; i >= start; i--) {
+            ranger[index] = i;
+            index++;
+        }
+
+        return ranger;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        return array[0] + array[1];
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        return array[array.length-1] * array[array.length-2];
     }
+
 }
